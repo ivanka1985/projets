e0849d7953095dcc6764185bf4fe368341ace331
IV <133577051+ivanka1985@users.noreply.github.com>
2023-09-07 11:20:30 +0200

    Add files via upload

--- coinsJS/script.js
+++ coinsJS/script.js
@@ -1,3 +1,22 @@
+let euro1 = document.getElementById("euro1");
+let euro2 = document.getElementById("euro2");
+let euro3 = document.getElementById("euro3");
+
+
+function lancerAnimation(){
+  euro1.classList.add("rot1");
+  euro2.classList.add("rot2");
+  euro3.classList.add("rot3");
+}
+
+function arreterAnimation(){
+euro1.classList.remove("rot1");
+euro2.classList.remove("rot2");
+euro3.classList.remove("rot3");
+}
+
+
+/******
 let animationPaused = false;
 let rot1 = document.querySelector('.rot1');
 let rot2 = document.querySelector('.rot2');
@@ -26,3 +45,5 @@ let resumeButton = document.querySelector('.btn:nth-child(2)');
 
 stopButton.addEventListener('click', stopAnimation);
 resumeButton.addEventListener('click', resumeAnimation);
+
+*****/
